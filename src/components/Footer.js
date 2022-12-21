bf5bccdeef3f67c586c4eb187f26b2f959037c3c
Rajat Soni <rajatsonibkn2002@gmail.com>
2022-12-21 12:18:21 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 const Footer = () => {
     return (
         //<footer className="footer">Made with 💜 by Anushka & Rajat</footer>
+        <footer className="footer">Footer Not Found</footer>
     )
 }
 
